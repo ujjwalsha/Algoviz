6ab17b9dcfd52786fe3f9800706e1e7c9ad07977
ujjwal kumar <91054640+ujjwalsha@users.noreply.github.com>
2023-11-14 21:13:38 +0530

    modification

---
+++ app.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".sec-text", {
+    strings: ["Algorithm Visualizer", "creativity", "Programming"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
---
+++ sorting/app.js
@@ -0,0 +1,27 @@
+
+// for slider
+
+      let mySlider = document.querySelector("#input");
+        let mySlider2 = document.querySelector("#range");
+
+        let value = document.querySelector(".result");
+        let value1 = document.querySelector(".resultSpeed");
+
+        mySlider.addEventListener("input", () =>{
+            value.innerText = mySlider.value;
+            bar.
+            // console.log(value.innerText);
+        });
+
+        mySlider2.addEventListener("input", () => {
+            value1.innerText = mySlider2.value;
+        });
+
+
+ let bar = document.querySelector('#bars');
+
+
+
+
+
+
