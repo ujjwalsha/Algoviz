4e3c1a1dacc671f6c800c076e1aaf6e8ec4bcbde
ujjwal kumar <91054640+ujjwalsha@users.noreply.github.com>
2023-11-15 22:08:09 +0530

    modify2

--- sorting/app.js
+++ sorting/app.js
@@ -5,17 +5,7 @@
         let value = document.querySelector(".result");
         let value1 = document.querySelector(".resultSpeed");
 
-        mySlider.addEventListener("input", () =>{
-            value.innerText = mySlider.value;
-            // console.log(value.innerText);
-        });
-        
-        mySlider2.addEventListener("input", () => {
-            value1.innerText = mySlider2.value;
-        });
-        
 
- 
  let block = document.querySelector('.sortBars');
  let containerBox = document.querySelector('.flex-container');
 let randomArray = document.getElementById('random-array');
@@ -23,7 +13,7 @@ let sort_btn = document.getElementById("bubble-sort");
 // let bubbleSort = document.getElementById('#bubble-sort');
 
 
-let min = 1;
+let min = 5;
 let max = mySlider.value;
 
 let numOfBars = mySlider.value;
@@ -31,13 +21,35 @@ console.log("value of slider" +numOfBars);
 
 let unsorted_array = new Array(numOfBars);
 
-//create random number
+mySlider.addEventListener("input", () => {
+    value.innerText = mySlider.value;
+    // min = mySlider.value;
+    // numOfBars = min;
+
+    // return numOfBars;
+    // console.log(min);
+    // console.log(numOfBars);    
+});
+
+
+
+mySlider2.addEventListener("input", () => {
+    value1.innerText = mySlider2.value;
+
+});
+
+
+
+
+//generate random number
 function randomNum(min, max){
-    return Math.floor(Math.random()*(max-min));
+    return Math.floor(Math.random()*(max-min)) + min;
 }
+
 
 // let value2 = randomNum(min, max);
 // console.log('value is ' +value2)
+
 
 //create random array
 function createRandomArray(){
@@ -50,15 +62,17 @@ function createRandomArray(){
 
 // let bar = document.querySelector('bar')
 
-// createRandomArray();
+// render the bars on the display
 function renderBars(array){
-    for(let i = 0; i <= array.length; i++){
+    for(let i = 0; i < array.length; i++){
         let bar = document.createElement("div");
         bar.classList.add("bar");
         bar.style.width = "2em";
+        bar.innerHTML = array[i];
+        bar.style.cssText = " color: black; padding: 0.5em; font-size:2em; "
         bar.style.backgroundColor = "aqua";
         bar.style.border = "1px solid blanchedalmond";
-        bar.style.height = array[i]*10 + "px";
+        bar.style.height = array[i]*25 + "px";
         containerBox.appendChild(bar);
     }
 
@@ -79,7 +93,7 @@ function bubbleSort(array){
 
     for(let i = 0; i < array.length; i++)
     {
-        for(let j = 0; j < array.length - i -1; j++)
+        for(let j = 0; j <= array.length - i -1; j++)
         {
             if(array[j] > array[j+1])
             {
@@ -87,26 +101,28 @@ function bubbleSort(array){
                 array[j] = array[j+1];
                 array[j+1] = temp;
 
-                bars[j].style.height = array[j]*10 + "px";
+                bars[j].style.height = array[j]*20 + "px";
                 bars[j].style.backgroundColor = "blanchedalmond";
-                // bars[j].innerText = array[j];
+                bars[j].innerHTML = array[j];
 
-                bars[j+1].style.height = array[j+1]*10 + "px";
+                bars[j+1].style.height = array[j+1]*20 + "px";
                 bars[j+1].style.backgroundColor = "blanchedalmond";
-                
-
+                bars[j+1].innerHTML = array[j+1];
+                // console.log(bars);
             }
         }
     }
-
     return array;
 }
 
 
 
 sort_btn.addEventListener("click", function(array){
+    
+
     let sorted_array = bubbleSort(unsorted_array);
-    // console.log(sorted_array);
+    console.log(sorted_array);
+    
 });
 
 
